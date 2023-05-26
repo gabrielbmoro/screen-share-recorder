f9aa97486c3acaedd25dd8a349f8bc6e12c106a8
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2023-05-26 19:01:55 -0300

    Suporte to handle intent action, start ou stop

--- screenShareTest/app/src/main/java/com/example/screensharetest/MainActivity.kt
+++ screenShareTest/app/src/main/java/com/example/screensharetest/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.example.screensharetest
 
-import android.content.ComponentName
-import android.content.Intent
-import android.content.ServiceConnection
 import android.media.projection.MediaProjectionManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.IBinder
-import android.util.Log
-import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.widget.Button
 import androidx.activity.result.ActivityResult
@@ -25,20 +19,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mediaProjectionManager: MediaProjectionManager
     private var screenSharePermissionResult: ActivityResult? = null
 
-    private var screenCaptureService: IScreenCaptureForegroundService? = null
-
-    private val serviceConnection = object : ServiceConnection {
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            screenCaptureService =
-                (service as? ScreenCaptureForegroundService.LocalBinder)?.service()
-        }
-
-        override fun onServiceDisconnected(name: ComponentName?) {
-            screenCaptureService = null
-        }
-    }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -47,43 +27,14 @@ class MainActivity : AppCompatActivity() {
         this.startButton = findViewById(R.id.start_button)
         this.stopButton = findViewById(R.id.stop_button)
 
-        bindService(
-            Intent(this, ScreenCaptureForegroundService::class.java),
-            serviceConnection,
-            BIND_AUTO_CREATE
-        )
-
         this.mediaProjectionManager = getSystemService(MediaProjectionManager::class.java)
-
-        this.surfaceView.holder.addCallback(
-            object : SurfaceHolder.Callback {
-                override fun surfaceCreated(holder: SurfaceHolder) {
-                    Log.d(TAG, "SurfaceView.surfaceCreated")
-                }
-
-                override fun surfaceChanged(
-                    holder: SurfaceHolder,
-                    format: Int,
-                    width: Int,
-                    height: Int
-                ) {
-                    Log.d(TAG, "SurfaceView.surfaceChanged")
-                }
-
-                override fun surfaceDestroyed(holder: SurfaceHolder) {
-                    Log.d(TAG, "SurfaceView.surfaceDestroyed")
-                }
-
-            }
-        )
 
         val launcher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
                 screenSharePermissionResult = result
                 if (screenSharePermissionResult?.resultCode == RESULT_OK && screenSharePermissionResult?.data != null) {
-                    val serviceIntent = ScreenCaptureForegroundService.createScreenCaptureIntent(
+                    val serviceIntent = ScreenCaptureForegroundService.createStartRecordingIntent(
                         context = this,
-                        surface = surfaceView.holder.surface,
                         resultCode = screenSharePermissionResult!!.resultCode,
                         resultData = screenSharePermissionResult!!.data!!
                     )
@@ -100,15 +51,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         stopButton.setOnClickListener {
-            screenCaptureService?.stopScreenShare()
+            val serviceIntent = ScreenCaptureForegroundService.createStopRecordingIntent(this)
+            startService(serviceIntent)
+
             startButton.isEnabled = true
             stopButton.isEnabled = false
         }
-    }
-
-    override fun onDestroy() {
-        unbindService(serviceConnection)
-
-        super.onDestroy()
     }
 }
--- screenShareTest/app/src/main/java/com/example/screensharetest/ScreenCaptureForegroundService.kt
+++ screenShareTest/app/src/main/java/com/example/screensharetest/ScreenCaptureForegroundService.kt
@@ -1,6 +1,7 @@
 package com.example.screensharetest
 
 import android.app.Activity
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
@@ -10,36 +11,86 @@ import android.hardware.display.DisplayManager
 import android.hardware.display.VirtualDisplay
 import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
-import android.os.Binder
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
 import android.util.Log
-import android.view.Surface
 import androidx.core.app.NotificationCompat
 
-interface IScreenCaptureForegroundService {
-    fun stopScreenShare()
-}
-
-class ScreenCaptureForegroundService : Service(), IScreenCaptureForegroundService {
+class ScreenCaptureForegroundService : Service() {
     private var virtualDisplay: VirtualDisplay? = null
     private var mediaProjection: MediaProjection? = null
 
-    override fun onBind(intent: Intent?): IBinder = LocalBinder()
+    override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val resultCode = intent?.getIntExtra(RESULT_CODE_KEY, Activity.RESULT_CANCELED)
-        val resultData = intent?.getParcelableExtra<Intent>(RESULT_DATA_KEY)
-        val surface = intent?.getParcelableExtra<Surface>(SURFACE_KEY)
+        when (intent?.action) {
+            START_RECORDING_INTENT_ACTION -> {
+                val notification = baseNotification()
 
-        if (surface != null && resultCode != null && resultData != null) {
-            startForegroundService(surface, resultCode, resultData)
+                startForeground(1, notification)
+
+                val mediaProjectionManager = getSystemService(MediaProjectionManager::class.java)
+
+                val resultCode = intent.getIntExtra(RESULT_CODE_KEY, Activity.RESULT_CANCELED)
+                val resultData = intent.getParcelableExtra<Intent>(RESULT_DATA_KEY)
+                mediaProjection =
+                    mediaProjectionManager.getMediaProjection(resultCode, resultData!!)
+
+                mediaProjection?.registerCallback(
+                    object : MediaProjection.Callback() {
+                        override fun onStop() {
+                            virtualDisplay?.release()
+                            Log.d(TAG, "MediaProjection.onStop()")
+                        }
+                    },
+                    Handler(Looper.myLooper()!!)
+                )
+
+                val metrics = resources.displayMetrics
+
+                virtualDisplay = mediaProjection?.createVirtualDisplay(
+                    "Screen 1",
+                    metrics.widthPixels,
+                    metrics.heightPixels,
+                    metrics.densityDpi,
+                    DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                    null,
+                    object : VirtualDisplay.Callback() {
+                        override fun onStopped() {
+                            Log.d(TAG, "VirtualDisplay.onStop()")
+                        }
+
+                        override fun onPaused() {
+                            Log.d(TAG, "VirtualDisplay.onPaused()")
+                        }
+
+                        override fun onResumed() {
+                            Log.d(TAG, "VirtualDisplay.onResumed()")
+                        }
+                    },
+                    null
+                )
+
+                return START_STICKY
+            }
+
+            STOP_RECORDING_INTENT_ACTION -> {
+                mediaProjection?.stop()
+                virtualDisplay?.release()
+
+                mediaProjection = null
+                virtualDisplay = null
+
+                stopSelf()
+                return START_NOT_STICKY
+            }
+
+            else -> return START_STICKY
         }
-        return START_STICKY
     }
 
-    private fun startForegroundService(surface: Surface, resultCode: Int, resultData: Intent) {
+    private fun baseNotification(): Notification {
         val channel = NotificationChannel(
             CHANNEL_ID,
             "Foreground Service",
@@ -48,51 +99,11 @@ class ScreenCaptureForegroundService : Service(), IScreenCaptureForegroundServic
         val manager = getSystemService(NotificationManager::class.java)
         manager.createNotificationChannel(channel)
 
-        val builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+        return NotificationCompat.Builder(applicationContext, CHANNEL_ID)
             .setContentTitle("Screen share")
             .setContentText("Running Media projection...")
             .setSmallIcon(R.drawable.ic_warning)
-
-
-        startForeground(1, builder.build())
-
-        val mediaProjectionManager = getSystemService(MediaProjectionManager::class.java)
-        mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, resultData)
-
-        mediaProjection?.registerCallback(
-            object : MediaProjection.Callback() {
-                override fun onStop() {
-                    virtualDisplay?.release()
-                    Log.d(TAG, "MediaProjection.onStop()")
-                }
-            },
-            Handler(Looper.myLooper()!!)
-        )
-
-        val metrics = resources.displayMetrics
-
-        virtualDisplay = mediaProjection?.createVirtualDisplay(
-            "Screen 1",
-            metrics.widthPixels,
-            metrics.heightPixels,
-            metrics.densityDpi,
-            DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-            surface,
-            object : VirtualDisplay.Callback() {
-                override fun onStopped() {
-                    Log.d(TAG, "VirtualDisplay.onStop()")
-                }
-
-                override fun onPaused() {
-                    Log.d(TAG, "VirtualDisplay.onPaused()")
-                }
-
-                override fun onResumed() {
-                    Log.d(TAG, "VirtualDisplay.onResumed()")
-                }
-            },
-            null
-        )
+            .build()
     }
 
     override fun onDestroy() {
@@ -100,36 +111,31 @@ class ScreenCaptureForegroundService : Service(), IScreenCaptureForegroundServic
         super.onDestroy()
     }
 
-
-    override fun stopScreenShare() {
-        mediaProjection?.stop()
-        virtualDisplay?.release()
-
-        mediaProjection = null
-        virtualDisplay = null
-    }
-
-
-    inner class LocalBinder : Binder() {
-        fun service(): IScreenCaptureForegroundService = this@ScreenCaptureForegroundService
-    }
-
     companion object {
         const val CHANNEL_ID = "ScreenCaptureServiceChannel"
-        private const val SURFACE_KEY = "surfaceKey"
+        private const val START_RECORDING_INTENT_ACTION = "start_recording"
+        private const val STOP_RECORDING_INTENT_ACTION = "stop_recording"
         private const val RESULT_CODE_KEY = "resultCodeKey"
         private const val RESULT_DATA_KEY = "resultDataKey"
 
-        fun createScreenCaptureIntent(
+        fun createStartRecordingIntent(
             context: Context,
-            surface: Surface,
             resultCode: Int,
             resultData: Intent
         ): Intent {
             return Intent(context, ScreenCaptureForegroundService::class.java).apply {
-                putExtra(SURFACE_KEY, surface)
                 putExtra(RESULT_CODE_KEY, resultCode)
                 putExtra(RESULT_DATA_KEY, resultData)
+
+                action = START_RECORDING_INTENT_ACTION
+            }
+        }
+
+        fun createStopRecordingIntent(
+            context: Context
+        ): Intent {
+            return Intent(context, ScreenCaptureForegroundService::class.java).apply {
+                action = STOP_RECORDING_INTENT_ACTION
             }
         }
     }
