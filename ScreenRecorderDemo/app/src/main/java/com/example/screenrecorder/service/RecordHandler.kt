e250d203f8ae4a59125df7b4035707580832dae4
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2023-05-28 19:18:57 -0300

    Renaming the directory to screen recorder demo

---
+++ ScreenRecorderDemo/app/src/main/java/com/example/screenrecorder/service/RecordHandler.kt
@@ -0,0 +1,139 @@
+package com.example.screenrecorder.service
+
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
+import android.media.MediaCodec
+import android.media.MediaCodecInfo
+import android.media.MediaFormat
+import android.media.MediaMuxer
+import android.media.projection.MediaProjection
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.DisplayMetrics
+import android.view.Surface
+
+class RecordHandler(
+    looper: Looper,
+    private val mediaProjection: MediaProjection,
+    outputFile: String,
+    private val displayMetrics: DisplayMetrics,
+) : Handler(looper) {
+
+    private val mediaCodec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_VIDEO_AVC)
+    private val mediaFormat = MediaFormat.createVideoFormat(
+        MediaFormat.MIMETYPE_VIDEO_AVC,
+        displayMetrics.widthPixels,
+        displayMetrics.heightPixels
+    ).apply {
+        setInteger(MediaFormat.KEY_BIT_RATE, 5_000_000)
+        setInteger(MediaFormat.KEY_FRAME_RATE, 30)
+        setInteger(
+            MediaFormat.KEY_COLOR_FORMAT,
+            MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface
+        )
+        setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 1)
+    }
+    private val mediaMuxer = MediaMuxer(outputFile, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
+    private val bufferInfo = MediaCodec.BufferInfo()
+
+    private var surface: Surface? = null
+
+    private var virtualDisplay: VirtualDisplay? = null
+
+    private fun setupMediaCodec() {
+        // Configure the MediaCodec encoder with the MediaFormat
+        mediaCodec.configure(mediaFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
+
+        // Create a surface from the MediaCodec encoder
+        surface = mediaCodec.createInputSurface()
+
+        // Start the MediaCodec encoder
+        mediaCodec.start()
+    }
+
+    override fun handleMessage(msg: Message) {
+        when (msg.what) {
+            START_RECORD_MESSAGE -> {
+                post {
+                    setupMediaCodec()
+
+                    virtualDisplay = mediaProjection.createVirtualDisplay(
+                        "virtual display",
+                        displayMetrics.widthPixels,
+                        displayMetrics.heightPixels,
+                        displayMetrics.densityDpi,
+                        DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                        surface,
+                        null,
+                        null
+                    )
+                    recordSurface()
+                }
+            }
+
+            STOP_RECORD_MESSAGE -> {
+                mediaCodec.signalEndOfInputStream()
+                mediaProjection.stop()
+                virtualDisplay?.release()
+                virtualDisplay = null
+                surface = null
+            }
+        }
+    }
+
+
+    private fun recordSurface() {
+        var videoTrackIndex: Int? = null
+
+        while (true) {
+            val encoderStatus = mediaCodec.dequeueOutputBuffer(bufferInfo, -1)
+            when {
+                // No output available yet, wait for the next buffer
+                encoderStatus == MediaCodec.INFO_TRY_AGAIN_LATER -> {
+                    continue
+                }
+
+                // The format of the output has changed (e.g., for the first frame), get the new format
+                encoderStatus == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED -> {
+                    val newFormat = mediaCodec.outputFormat
+                    videoTrackIndex = mediaMuxer.addTrack(newFormat)
+                    mediaMuxer.start()
+                }
+
+                // Encode and write the buffer to the MediaMuxer
+                (encoderStatus >= 0) -> {
+                    mediaCodec.getOutputBuffer(encoderStatus)?.let { encodedData ->
+                        encodedData.position(bufferInfo.offset)
+                        encodedData.limit(bufferInfo.offset + bufferInfo.size)
+                        mediaMuxer.writeSampleData(videoTrackIndex!!, encodedData, bufferInfo)
+                    }
+
+                    mediaCodec.releaseOutputBuffer(encoderStatus, false)
+
+                    if ((bufferInfo.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+                        mediaCodec.stop()
+                        mediaCodec.release()
+                        mediaMuxer.stop()
+                        mediaMuxer.release()
+                        return
+                    }
+                }
+            }
+        }
+    }
+
+
+    companion object {
+        private const val START_RECORD_MESSAGE = 0
+        private const val STOP_RECORD_MESSAGE = 1
+
+        fun startMessage() = Message().apply {
+            what = START_RECORD_MESSAGE
+        }
+
+        fun stopMessage() = Message().apply {
+            what = STOP_RECORD_MESSAGE
+        }
+    }
+}
---
+++ ScreenRecorderDemo/app/src/main/java/com/example/screenrecorder/service/ScreenCaptureForegroundService.kt
@@ -0,0 +1,115 @@
+package com.example.screenrecorder.service
+
+import android.app.Activity
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.media.projection.MediaProjectionManager
+import android.os.Environment
+import android.os.HandlerThread
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.example.screenrecorder.R
+import java.io.File
+
+class ScreenCaptureForegroundService : Service() {
+
+    private var recordHandler: RecordHandler? = null
+    private val recordThread: HandlerThread = HandlerThread("record handler")
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.action) {
+            START_RECORDING_INTENT_ACTION -> {
+                val notification = baseNotification()
+
+                startForeground(1, notification)
+
+                val mediaProjectionManager = getSystemService(MediaProjectionManager::class.java)
+
+                val resultCode = intent.getIntExtra(RESULT_CODE_KEY, Activity.RESULT_CANCELED)
+                val resultData = intent.getParcelableExtra<Intent>(RESULT_DATA_KEY)
+                val mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, resultData!!)
+
+                recordThread.start()
+
+                this.recordHandler = RecordHandler(
+                    recordThread.looper,
+                    mediaProjection,
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).absolutePath + File.separator + "recorded.mp4",
+                    resources.displayMetrics
+                )
+                this.recordHandler?.handleMessage(RecordHandler.startMessage())
+
+                return START_STICKY
+            }
+
+            STOP_RECORDING_INTENT_ACTION -> {
+                this.recordHandler?.handleMessage(RecordHandler.stopMessage())
+
+                stopSelf()
+                return START_NOT_STICKY
+            }
+
+            else -> return START_STICKY
+        }
+    }
+
+    private fun baseNotification(): Notification {
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            "Foreground Service",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(channel)
+
+        return NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setContentTitle("Screen share")
+            .setContentText("Running Media projection...")
+            .setSmallIcon(R.drawable.ic_warning)
+            .build()
+    }
+
+    override fun onDestroy() {
+        stopForeground(STOP_FOREGROUND_REMOVE)
+
+        recordThread.quitSafely()
+        recordHandler = null
+
+        super.onDestroy()
+    }
+
+    companion object {
+        const val CHANNEL_ID = "ScreenCaptureServiceChannel"
+        private const val START_RECORDING_INTENT_ACTION = "start_recording"
+        private const val STOP_RECORDING_INTENT_ACTION = "stop_recording"
+        private const val RESULT_CODE_KEY = "resultCodeKey"
+        private const val RESULT_DATA_KEY = "resultDataKey"
+
+        fun createStartRecordingIntent(
+            context: Context,
+            resultCode: Int,
+            resultData: Intent
+        ): Intent {
+            return Intent(context, ScreenCaptureForegroundService::class.java).apply {
+                putExtra(RESULT_CODE_KEY, resultCode)
+                putExtra(RESULT_DATA_KEY, resultData)
+
+                action = START_RECORDING_INTENT_ACTION
+            }
+        }
+
+        fun createStopRecordingIntent(
+            context: Context
+        ): Intent {
+            return Intent(context, ScreenCaptureForegroundService::class.java).apply {
+                action = STOP_RECORDING_INTENT_ACTION
+            }
+        }
+    }
+}
