63af9a527ef73350a1e32648cce1acc579325d1e
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2023-06-03 18:48:48 -0300

    Using scope functions inside of MediaRecorder

--- ScreenRecorderDemo/app/src/main/java/com/example/screenrecorder/service/RecordManager.kt
+++ ScreenRecorderDemo/app/src/main/java/com/example/screenrecorder/service/RecordManager.kt
@@ -19,7 +19,7 @@ class RecordManager(
 
     private var mediaRecorder: MediaRecorder? = null
     private var virtualDisplay: VirtualDisplay? = null
-    private var surface: Surface? = null
+    private var commonSurface: Surface? = null
 
     fun start(context: Context) {
         try {
@@ -31,76 +31,82 @@ class RecordManager(
                 MediaRecorder()
             }
 
-            mediaRecorder!!.setVideoSource(MediaRecorder.VideoSource.SURFACE)
-            mediaRecorder!!.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            mediaRecorder!!.run {
+                setVideoSource(MediaRecorder.VideoSource.SURFACE)
+                setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
 
-            mediaRecorder!!.setOutputFile(outputFile)
-            Timber.d("Output file -> $outputFile")
+                setOutputFile(outputFile)
+                Timber.d("Output file -> $outputFile")
 
-            val widthPixels = if (displayMetrics.widthPixels > MAX_WIDTH_SUPPORTED) {
-                Timber.d("Display metrics exceed the width limit...")
-                MAX_WIDTH_SUPPORTED
-            } else {
-                displayMetrics.widthPixels
+                val widthPixels = widthNormalized()
+                val heightPixels = heightNormalized()
+
+                setVideoSize(widthPixels, heightPixels)
+                Timber.d("Final video dimensions ->  $widthPixels x $heightPixels")
+
+                setVideoEncoder(MediaRecorder.VideoEncoder.H264)
+
+                prepare()
+                Timber.d("Media recorder prepared...")
+
+                commonSurface = surface
+
+                virtualDisplay = mediaProjection.createVirtualDisplay(
+                    VIRTUAL_DISPLAY_NAME,
+                    widthPixels,
+                    heightPixels,
+                    displayMetrics.densityDpi,
+                    DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                    commonSurface,
+                    object : VirtualDisplay.Callback() {
+                        override fun onPaused() {
+                            Timber.d("Virtual display paused...")
+                        }
+
+                        override fun onResumed() {
+                            Timber.d("Virtual display resumed...")
+
+                            this@run.start()
+                            Timber.d("Record started...")
+                            callback.onRecordStarted()
+                        }
+
+                        override fun onStopped() {
+                            Timber.d("Virtual display stopped...")
+                        }
+                    },
+                    null
+                )
             }
-
-            val heightPixels = if (displayMetrics.heightPixels > MAX_HEIGHT_SUPPORTED) {
-                Timber.d("Display metrics exceed the height limit...")
-                MAX_HEIGHT_SUPPORTED
-            } else {
-                displayMetrics.heightPixels
-            }
-
-            mediaRecorder!!.setVideoSize(widthPixels, heightPixels)
-            Timber.d("Final video dimensions ->  $widthPixels x $heightPixels")
-
-            mediaRecorder!!.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
-
-            mediaRecorder!!.prepare()
-            Timber.d("Media recorder prepared...")
-
-            surface = mediaRecorder!!.surface
-
-            virtualDisplay = mediaProjection.createVirtualDisplay(
-                VIRTUAL_DISPLAY_NAME,
-                widthPixels,
-                heightPixels,
-                displayMetrics.densityDpi,
-                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-                surface,
-                object : VirtualDisplay.Callback() {
-                    override fun onPaused() {
-                        Timber.d("Virtual display paused...")
-                    }
-
-                    override fun onResumed() {
-                        Timber.d("Virtual display resumed...")
-
-                        mediaRecorder!!.start()
-                        Timber.d("Record started...")
-                        callback.onRecordStarted()
-                    }
-
-                    override fun onStopped() {
-                        Timber.d("Virtual display stopped...")
-                    }
-                },
-                null
-            )
         } catch (exception: Exception) {
             Timber.e(exception)
             callback.onError(exception)
         }
     }
 
+    private fun heightNormalized() = if (displayMetrics.heightPixels > MAX_HEIGHT_SUPPORTED) {
+        Timber.d("Display metrics exceed the height limit...")
+        MAX_HEIGHT_SUPPORTED
+    } else {
+        displayMetrics.heightPixels
+    }
+
+    private fun widthNormalized() = if (displayMetrics.widthPixels > MAX_WIDTH_SUPPORTED) {
+        Timber.d("Display metrics exceed the width limit...")
+        MAX_WIDTH_SUPPORTED
+    } else {
+        displayMetrics.widthPixels
+    }
+
     fun stop() {
         try {
-            mediaRecorder?.stop()
-            Timber.d("Record stopped...")
+            mediaRecorder?.run {
+                stop()
+                Timber.d("Record stopped...")
 
-            mediaRecorder?.release()
-            Timber.d("Media recorder released...")
-
+                release()
+                Timber.d("Media recorder released...")
+            }
             mediaRecorder = null
 
             virtualDisplay?.release()
